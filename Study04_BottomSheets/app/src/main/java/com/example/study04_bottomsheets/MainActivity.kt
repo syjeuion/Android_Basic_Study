52ad4e087863f0e39159be4a8855b3a2de3fb698
syjeuion <105288129+syjeuion@users.noreply.github.com>
2023-11-19 19:03:22 +0900

    [FEAT] BottomSheet 구현

--- Study04_BottomSheets/app/src/main/java/com/example/study04_bottomsheets/MainActivity.kt
+++ Study04_BottomSheets/app/src/main/java/com/example/study04_bottomsheets/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.study04_bottomsheets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.study04_bottomsheets.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Expanding BottomSheet
+        BottomSheetBehavior.from(binding.coordinator).apply {
+//            maxHeight = 700
+            peekHeight = 200
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+
+        //BottomSheetDialogFragment
+//        binding.btn.setOnClickListener {
+//            val bottomSheetFragmet = BottomSheetFragment()
+//            bottomSheetFragmet.show(supportFragmentManager, bottomSheetFragmet.tag)
+//        }
+
     }
 }
